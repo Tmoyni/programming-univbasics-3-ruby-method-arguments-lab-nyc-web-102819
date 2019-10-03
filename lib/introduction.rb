0db3f40328f05fc4c53228f9d4ed60f731164426
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-03 03:48:12 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,7 +11,7 @@ def introduction_with_language(name, language)
     puts "Hi, name name is #{name} and I am learning to program in #{language}."
 end
     
-def introduction_with_language_optional(name, language= Ruby)
+def introduction_with_language_optional(name, language= ruby)
     puts "Hi, name name is #{name} and I am learning to program in #{language}."
   end
 
